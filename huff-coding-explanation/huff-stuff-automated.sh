0d64768183871a8bbb89623a0f741fa57722b560
theta291 <theta291@gmail.com>
2022-05-24 16:16:55 -0700

    Changed package to python graphiz

--- huff-coding-explanation/huff-stuff-automated.sh
+++
@@ -1,4 +0,0 @@
-git clone -b huff https://github.com/Theta291/encoding.git
-conda env create -f ./encoding/huff-coding-explanation/huff-ipynb-env.yml
-source activate huff-coding
-jupyter notebook ./encoding/huff-coding-explanation/huffman-coding.ipynb
